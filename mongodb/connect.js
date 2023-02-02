c0e1c62277aec20f46418a29c6d411c208e50933
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-02-02 08:18:16 +0700

    feat: add initialize and connect with mongodb

---
+++ mongodb/connect.js
@@ -0,0 +1,9 @@
+import mongoose, { connect } from "mongoose";
+
+const connectDB = (url) => {
+  mongoose.set('strictQuery', true);
+
+  mongoose.connect(url).then(() => console.log('MongoDB Connected')).catch((err) => console.log(err));
+}
+
+export default connectDB;
