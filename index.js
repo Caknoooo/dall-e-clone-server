e735e9d526038c66e0f2bdd65efa5767e9847fc5
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-02-03 16:18:09 +0700

    vercel: trigger deployment

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
-// import postRoutes from './routes/postRoutes.js';
-// import dalleRoutes from './routes/dalleRoutes.js';
-
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
 
@@ -13,8 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-//app.use('api/v1/post', postRoutes);
-// app.use('api/v1/dalle', dalleRoutes);
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
   res.send('Hello World')
