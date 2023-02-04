b1a4705f7578ac0f751b8abdf64e06831bbffa06
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-02-04 16:08:01 +0700

    fix: fiex post Routes and trigger deployment

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -9,9 +9,9 @@ dotenv.config();
 const router = express.Router();
 
 cloudinary.config({
-  cloud_name: process.env.COUDINARY_CLOUD_NAME,
-  api_key: process.env.COUDINARY_API_KEY,
-  api_secret: process.env.COUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // GET ALL POSTS
